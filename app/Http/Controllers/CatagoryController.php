2adb723388374b6a13df7f9d162a5e1aea053021
azriel123123 <mazrielilham4@gmail.com>
2024-02-16 13:24:41 +0700

    new

--- app/Http/Controllers/CatagoryController.php
+++ app/Http/Controllers/CatagoryController.php
@@ -13,7 +13,7 @@ class CatagoryController extends Controller
      */
     public function index()
     {
-        //
+        return view('catagory.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class CatagoryController extends Controller
      */
     public function create()
     {
-        //
+        // 
     }
 
     /**
