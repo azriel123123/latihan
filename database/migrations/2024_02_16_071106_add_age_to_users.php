798c5f21d8e9a3a4210faa128cfe561b8b735451
azriel123123 <mazrielilham4@gmail.com>
2024-02-16 14:15:33 +0700

    new

--- database/migrations/2024_02_16_071106_add_age_to_users.php
+++ database/migrations/2024_02_16_071106_add_age_to_users.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('age');
+
         });
     }
 
@@ -26,7 +27,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('age');
         });
     }
 };
