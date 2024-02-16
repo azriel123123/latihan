5e3f6e57b76a928f862735577b7160aa6c84de03
azriel123123 <mazrielilham4@gmail.com>
2024-02-16 14:37:38 +0700

    new

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,4 +4,23 @@
 <div class="container d-flex justify-content-end">
     <a href="{{ route('admin.create') }}" class="btn btn-primary">Create Admin</a>
 </div>
+
+{{-- Tabel User --}}
+<div class="container text-center">
+    <div class="fw-bold">USER</div>
+    <table class="table">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Gender</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</div>
 @endsection
